0bd729dcdea27dc69229657b75c6380d2624e002
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2025-07-28 10:16:16 +0200

    🔧 readd custom & and fix mason-lspconfig

--- init.lua
+++ init.lua
@@ -597,13 +597,16 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'prettierd', -- Used to format typescript
-        'ruff-lsp',
+        'ruff',
         'hadolint',
         'markdownlint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = {},
+        automatic_enable = true,
+        automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
@@ -885,7 +888,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
